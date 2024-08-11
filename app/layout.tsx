ec114ddb77a772e206d2ddda1512c07c10df92ae
mikenegh <mikenegh@gmail.com>
2024-08-11 13:36:59 +0000

    First Message

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlex = IBM_Plex_Serif({ subsets: ["latin"], variable: '--font-ibm-plex-serif', weight: ['400', '700'] })
+
+export const metadata: Metadata = {
+  title: "Fetan",
+  description: "Fetan is a modern banking app for everyone",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlex.variable}`}>{children}</body>
+    </html>
+  );
+} 
