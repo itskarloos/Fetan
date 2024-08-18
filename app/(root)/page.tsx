52c0a7aaaa75032a04d0df51cc8e056d0b2778d0
mikenegh <mikenegh@gmail.com>
2024-08-18 11:34:01 +0000

    Initialize FrontEnd Total Balance

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalancebox from "@/components/TotalBalancebox";
+import React from "react";
 
 const page = () => {
+  const loggedIn = { firstName: "Michael" };
   return (
-    <div>page</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalancebox
+            accounts = {[]}
+            totalBanks = {1}
+            totalCurrentBalance = {1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
